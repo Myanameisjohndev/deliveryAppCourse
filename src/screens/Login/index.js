266063f916536adcaeda40feb703bc851fee4edc
Myanameisjohndev <joaoantonio15161516@gmail.com>
2022-06-05 14:10:30 -0300

    Update project

--- src/screens/Login/index.js
+++ src/screens/Login/index.js
@@ -1,24 +1,13 @@
 import React, { useState} from 'react';
-import { View, StyleSheet } from 'react-native';
-import Input from '../../components/Input';
-import { OnChangeText } from '../../components/Input/styles';
+import { TextInput } from 'react-native';
 import { Background } from '../../globalstyles';
 
 const Login = () => {
 
-  const [senha, setSenha] = useState("oi");
-  const [number, onChangeNumber] = React.useState(null);
 
   return (
     <Background >
-      <Input 
-      iconName="Eye" 
-      placeholder="Senha" 
-      visible={false} 
-      textInput={senha}
-      setTextInput={setSenha}
-      />
-
+      <TextInput placeholder='teste'/>
     </Background>
   );
 }
