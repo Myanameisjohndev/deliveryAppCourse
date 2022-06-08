95c3183b7469c407cbdde76dcbd99d6db4c2538a
Myanameisjohndev <joaoantonio15161516@gmail.com>
2022-06-07 21:27:05 -0300

    listing products

--- src/screens/Home/index.js
+++ src/screens/Home/index.js
@@ -4,16 +4,45 @@ import Search from '../../components/Search';
 import { useAppContext } from '../../context';
 import { Background, Content, Header } from '../../globalstyles';
 import Product from '../../components/Product';
+import { FlatList } from 'react-native';
 
 const Home = () => {
 
   const { user } = useAppContext();
   const [searchValue, setSearchValue] = useState('');
 
-  const [ products, setProducts ] = useState([
+  const [products, setProducts] = useState([
     {
       url: "https://www.manollopizzaria.com.br/wp-content/uploads/2021/02/X_TUDO_DE_HAMBURGUER1-1-500x320.jpg",
       id: 1,
+      name: "X-tudo",
+      price: "17.90",
+      description: "Pão, bife, salada, ovo, bacon, mussarela, presunto, milho e batata."
+    },
+    {
+      url: "https://www.manollopizzaria.com.br/wp-content/uploads/2021/02/X_TUDO_DE_HAMBURGUER1-1-500x320.jpg",
+      id: 2,
+      name: "X-tudo",
+      price: "17.90",
+      description: "Pão, bife, salada, ovo, bacon, mussarela, presunto, milho e batata."
+    },
+    {
+      url: "https://www.manollopizzaria.com.br/wp-content/uploads/2021/02/X_TUDO_DE_HAMBURGUER1-1-500x320.jpg",
+      id: 3,
+      name: "X-tudo",
+      price: "17.90",
+      description: "Pão, bife, salada, ovo, bacon, mussarela, presunto, milho e batata."
+    },
+    {
+      url: "https://www.manollopizzaria.com.br/wp-content/uploads/2021/02/X_TUDO_DE_HAMBURGUER1-1-500x320.jpg",
+      id: 4,
+      name: "X-tudo",
+      price: "17.90",
+      description: "Pão, bife, salada, ovo, bacon, mussarela, presunto, milho e batata."
+    },
+    {
+      url: "https://www.manollopizzaria.com.br/wp-content/uploads/2021/02/X_TUDO_DE_HAMBURGUER1-1-500x320.jpg",
+      id: 5,
       name: "X-tudo",
       price: "17.90",
       description: "Pão, bife, salada, ovo, bacon, mussarela, presunto, milho e batata."
@@ -34,7 +63,13 @@ const Home = () => {
           onChangeText={setSearchValue}
           value={searchValue}
         />
-        <Product data={products[0]}/>
+        <FlatList
+          showsVerticalScrollIndicator={false}
+          contentContainerStyle={{ paddingVertical: 20 }}
+          data={products}
+          keyExtractor={(item) => item.id}
+          renderItem={({ item }) => <Product data={item} />}
+        />
       </Content>
     </Background>
   );
