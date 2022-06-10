b4ffa4e2fbda1a1e3ff3447ac6c186cbf168e9cc
Myanameisjohndev <joaoantonio15161516@gmail.com>
2022-06-09 22:11:51 -0300

    Creating quantity component

--- src/screens/SelectedProduct/index.js
+++ src/screens/SelectedProduct/index.js
@@ -1,18 +1,54 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useRoute } from '@react-navigation/native';
-import { Background, Content, Header } from '../../globalstyles';
-import { ContainerImage, Image, ScrollDescription, ContentAlign, Title, Text} from './styles';
+import { Background, Content, Header, PressButton } from '../../globalstyles';
+import {
+    ContainerImage,
+    Image,
+    ScrollDescription,
+    ContentAlign,
+    Title,
+    Text,
+    ColumButtons,
+    QuantityContainer,
+    Value
+} from './styles';
+import Add from '../../assets/add.svg';
+import Less from '../../assets/less.svg';
 
 const SelectedProduct = () => {
     const { item } = useRoute().params;
+    const [value, setValue] = useState(1);
 
-    return(
+    const addQuantity = () => {
+        setValue(value + 1);
+    }
+    const lessQuantity = () => {
+        if(value > 1){
+            setValue(value - 1);
+        }
+    }
+
+    const Quantity = () => {
+        return (
+            <QuantityContainer>
+               <PressButton onPress={lessQuantity}>
+                    <Less />
+                </PressButton>
+                    <Value>{value}</Value>
+                <PressButton onPress={addQuantity}>
+                    <Add />
+                </PressButton>
+            </QuantityContainer>
+        )
+    }
+
+    return (
         <Background>
             <Header>
             </Header>
             <Content>
                 <ContainerImage>
-                    <Image source={{uri: item.url}}/>
+                    <Image source={{ uri: item.url }} />
                 </ContainerImage>
                 <ScrollDescription>
                     <ContentAlign>
@@ -20,6 +56,9 @@ const SelectedProduct = () => {
                         <Text>{item.description}</Text>
                     </ContentAlign>
                 </ScrollDescription>
+                <ColumButtons>
+                    <Quantity />
+                </ColumButtons>
             </Content>
         </Background>
     )
--- src/screens/SelectedProduct/styles.js
+++ src/screens/SelectedProduct/styles.js
@@ -42,3 +42,29 @@ export const ContentAlign = styled.View`
    align-self: center;
    width: 90%;
 `;
+export const ColumButtons = styled.View`
+   align-self: center;
+   width: 90%;
+   margin-top: ${height * 0.43}px;
+   position: absolute;
+   /* background-color: blue; */
+   min-height: 100px;
+`;
+
+export const QuantityContainer = styled.View`
+   background-color: ${({theme})=>theme.COLORS.TEXT};
+   width: 49.2%;
+   height: 70px;
+   border-radius: 12px;
+   margin-bottom: 7px;
+   flex-direction: row;
+   justify-content: space-around;
+   align-items: center;
+   align-self: center;
+`;
+
+export const Value = styled.Text`
+    font-size: 22px;
+    font-weight: bold;
+    color: ${({theme})=>theme.COLORS.BUTTON};
+`;
